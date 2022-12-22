ad78ea7dcbdd6dd2fd04e2475e1161e87488eb78
Boris Karakolev <boriskarakolev@hotmail.com>
2022-12-22 18:10:42 +0100

    Design patterns with TS

---
+++ maps/src/Company.ts
@@ -0,0 +1,30 @@
+import { faker } from "@faker-js/faker";
+import { Mappable } from "./CustomMap";
+
+export class Company implements Mappable {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  color: string = 'green'
+
+  constructor() {
+    this.name = faker.company.name();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `
+    <div>
+     <h1>Company Name: ${this.name}</h1>
+     <h3>Catch Phrase is ${this.catchPhrase}</h3>
+    </div>
+    `;
+  }
+}
